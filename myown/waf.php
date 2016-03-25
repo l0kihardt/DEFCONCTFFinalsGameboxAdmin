d757427ed44fddef452d6b14d5cfb681e8472277
l0kihardt <nine.twelve@foxmail.com>
2016-03-25 16:11:50 +0800

    latest

---
+++ myown/waf.php
@@ -0,0 +1,130 @@
+<?php
+$_POST = dbhold($_POST);
+$_GET = dbhold($_GET);
+$_COOKIE = dbhold($_COOKIE);
+
+//exc are the arrays in variables
+function dbhold($str, $exc = array()){
+	if(is_array($str)){
+		foreach($str as $k => $v){
+			$str[$k] = in_array($k, $exc) ? dbhold($v, 'all') : dbhold($v);
+		}
+	}	
+	else{
+		$str = $exc == 'all' ? CheckSql($str) : CheckSql(htmlspecialchars($str));
+	}
+	return $str;
+}
+
+function CheckSql($db_string,$querytype='select')
+{
+        $clean = '';
+        $error='';
+        $old_pos = 0;
+        $pos = -1;
+        $userIP = GetIP();
+        $getUrl = GetCurUrl();
+
+        //如果是普通查询语句，直接过滤一些特殊语法
+        if($querytype=='select')
+        {
+            $notallow1 = "[^0-9a-z@\._-]{1,}(union|sleep|benchmark|load_file|outfile)[^0-9a-z@\.-]{1,}";
+            //$notallow2 = "--|/\*";
+            if(preg_match("/".$notallow1."/i", $db_string))
+            {
+                fputs(fopen($log_file,'a+'),"$userIP||$getUrl||$db_string||SelectBreak\r\n");
+                exit("<font size='5' color='red'>Safe Alert: Request Error step 1 !</font>");
+            }
+        }
+
+        //完整的SQL检查
+        while (TRUE)
+        {
+            $pos = strpos($db_string, '\'', $pos + 1);
+            if ($pos === FALSE)
+            {
+                break;
+            }
+            $clean .= substr($db_string, $old_pos, $pos - $old_pos);
+            while (TRUE)
+            {
+                $pos1 = strpos($db_string, '\'', $pos + 1);
+                $pos2 = strpos($db_string, '\\', $pos + 1);
+                if ($pos1 === FALSE)
+                {
+                    break;
+                }
+                elseif ($pos2 == FALSE || $pos2 > $pos1)
+                {
+                    $pos = $pos1;
+                    break;
+                }
+                $pos = $pos2 + 1;
+            }
+            $clean .= '$s$';
+            $old_pos = $pos + 1;
+        }
+        $clean .= substr($db_string, $old_pos);
+        $clean = trim(strtolower(preg_replace(array('~\s+~s' ), array(' '), $clean)));
+        
+        if (strpos($clean, '@') !== FALSE  OR strpos($clean,'char(')!== FALSE OR strpos($clean,'"')!== FALSE 
+        OR strpos($clean,'$s$$s$')!== FALSE)
+        {
+            $fail = TRUE;
+            if(preg_match("#^create table#i",$clean)) $fail = FALSE;
+            $error="unusual character";
+        }
+
+        //老版本的Mysql并不支持union，常用的程序里也不使用union，但是一些黑客使用它，所以检查它
+        if (strpos($clean, 'union') !== FALSE && preg_match('~(^|[^a-z])union($|[^[a-z])~is', $clean) != 0)
+        {
+            $fail = TRUE;
+            $error="union detect";
+        }
+
+        //发布版本的程序可能比较少包括--,#这样的注释，但是黑客经常使用它们
+        elseif (strpos($clean, '/*') > 2 || strpos($clean, '--') !== FALSE || strpos($clean, '#') !== FALSE)
+        {
+            $fail = TRUE;
+            $error="comment detect";
+        }
+
+        //这些函数不会被使用，但是黑客会用它来操作文件，down掉数据库
+        elseif (strpos($clean, 'sleep') !== FALSE && preg_match('~(^|[^a-z])sleep($|[^[a-z])~is', $clean) != 0)
+        {
+            $fail = TRUE;
+            $error="slown down detect";
+        }
+        elseif (strpos($clean, 'benchmark') !== FALSE && preg_match('~(^|[^a-z])benchmark($|[^[a-z])~is', $clean) != 0)
+        {
+            $fail = TRUE;
+            $error="slown down detect";
+        }
+        elseif (strpos($clean, 'load_file') !== FALSE && preg_match('~(^|[^a-z])load_file($|[^[a-z])~is', $clean) != 0)
+        {
+            $fail = TRUE;
+            $error="file fun detect";
+        }
+        elseif (strpos($clean, 'into outfile') !== FALSE && preg_match('~(^|[^a-z])into\s+outfile($|[^[a-z])~is', $clean) != 0)
+        {
+            $fail = TRUE;
+            $error="file fun detect";
+        }
+
+        //老版本的MYSQL不支持子查询，我们的程序里可能也用得少，但是黑客可以使用它来查询数据库敏感信息
+        elseif (preg_match('~\([^)]*?select~is', $clean) != 0)
+        {
+            $fail = TRUE;
+            $error="sub select detect";
+        }
+        if (!empty($fail))
+        {
+            fputs(fopen($log_file,'a+'),"$userIP||$getUrl||$db_string||$error\r\n");
+            exit("<font size='5' color='red'>Safe Alert: Request Error step 2!</font>");
+        }
+        else
+        {
+            return $db_string;
+        }
+}
+
